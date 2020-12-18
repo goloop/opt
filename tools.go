a835a42f8c557e7694bee9490fbf70a7a8bd8211
goloop <goloop.one@gmail.com>
2020-12-18 19:55:07 +0200

    Lint

--- tools.go
+++ tools.go
@@ -113,7 +113,7 @@ func splitN(str, sep string, n int) (r []string) {
 				r = append(r, tmp)
 				tmp = ""
 				if n > 0 && n == len(r)+1 {
-					tmp = string(str[endpoint:len(str)])
+					tmp = str[endpoint:]
 					break
 				}
 				continue
