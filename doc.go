2af0e6bae263b99d8f3dc1ce4dd0313425518791
valsorym <valsorym.e@gmail.com>
2022-05-24 17:23:06 +0300

    Fix bugs

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 // Package opt implements methods for manage arguments of the command-line.
 package opt
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 // Version returns the version of the module.
 func Version() string {
